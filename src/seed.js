96a103d73f565b3715f7614cf602c08fe715ce97
Ken Howard <ken@simplicatedweb.com>
2016-01-05 22:23:31 -0800

    Added seed data

---
+++ src/seed.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var Todo = require('./models/todo');
+
+var todos = [
+	'Feed Dog',
+	'Walk Dog',
+	'Pet Dog'
+];
+
+todos.forEach(function (t, index) {
+  Todo.find({ 'name': t }, function(err, todos) {
+  	if (!err && !todos.length) {
+      todo.create({ completed: false, name: t });
+  	}
+  });
+});
